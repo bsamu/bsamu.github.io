aa1a5df88c7ef00853183a9977b914fbbe2d46dd
bsamu <bsamuel0220@gmail.com>
2021-10-22 20:02:53 +0200

    added comments in .js, .css

--- you-shall-not-pass/script.js
+++ you-shall-not-pass/script.js
@@ -6,16 +6,16 @@ var height1 = 0;
 function smth () {
     console.log(height1);
     console.log(width1);
-    buttonSelect.style.setProperty('left', `${width1}px`);
-    buttonSelect.style.setProperty('top', `${height1}px`);
-    buttonSelect.style.setProperty('transform', 'none');
+    buttonSelect.style.setProperty('left', `${width1}px`); // 'margin-left', mértékegység vw volt
+    buttonSelect.style.setProperty('top', `${height1}px`); // 'margin-top', mértékegység vh volt
+    buttonSelect.style.setProperty('transform', 'none'); // itt már nem kell korrekció, nem volt itt semmi
 }
 
 function calculate1() {
-    const maxWidth = window.innerWidth-150; // kép szélességével korrigál
-    const maxHeight = window.innerHeight-150; // kép magasságával korrigál
-    width1 = Math.floor(Math.random() * maxWidth);
-    height1 = Math.floor(Math.random() * maxHeight);
+    const maxWidth = window.innerWidth-150; // kép szélességével korrigál, nem volt itt semmi előtte
+    const maxHeight = window.innerHeight-150; // kép magasságával korrigál, nem volt itt semmi előtte
+    width1 = Math.floor(Math.random() * maxWidth); // maxWidth helyett egy korrigációs szám volt
+    height1 = Math.floor(Math.random() * maxHeight); // maxHeight helyett egy korrigációs szám volt
     return smth();
 }
 
